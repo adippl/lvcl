cfb78d0bdec922d643f4d1ca942cd8d32567d7ab
ev <adam.prycki@gmail.com>
2020-12-07 08:39:29 +0100

    LICENSE, gpl headers and some exchange progress

--- conf.go
+++ conf.go
@@ -1,3 +1,21 @@
+/*
+ *  lvcl is a simple program clustering libvirt servers
+ *  Copyright (C) 2020 Adam Prycki (email: adam.prycki@gmail.com)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package main
 
 import "fmt"
@@ -5,6 +23,8 @@ import "os"
 import "encoding/json"
 import "io/ioutil"
 import "errors"
+import "crypto/sha256"
+import "io"
 
 const( confDir="./" )
 const( confFile="cluster.json" )
@@ -25,8 +45,12 @@ type Conf struct {
 	HwCpuMax uint
 	VMemMax uint
 	HwMemMax uint
-
+	
+	HeartbeatInterval uint
+	ClusterTickInterval uint
 	ConfFileHash string `json:"-"`
+	ConfFileHashRaw []byte `json:"-"`
+	MyHostname string `json:"-"`
 	}
 
 
@@ -54,12 +78,30 @@ func confLoad(){
 		fmt.Println("ERR reading cluster.json")
 		os.Exit(10);}
 		
+	config.MyHostname,err=os.Hostname()
+	if(err != nil){
+		panic(err)}
+	
+	/* TODO DEBUG 
+	 * this codes tests if current hosts is in config file. 
+	 * exits program if host is not in config
+	_,err=config.getNodebyHostname(&config.MyHostname)
+	if(err == nil){
+		fmt.Println("DEBUG err controller is started on hostname %s which doesn't exists in cluster configuration file\n",vm.DomainDefinition)
+		os.Exit()}
+	 */
+	
+	var h=sha256.New()
+	if _,err:=io.Copy(h,file);err!=nil {
+		fmt.Println("ERR hashing cluster.json")}
+	config.ConfFileHash=fmt.Sprintf("%x",h.Sum(nil))
+	config.ConfFileHashRaw=h.Sum(nil)
+		
+	
 	json.Unmarshal(raw,&config)
-
-	//VMReadFile("domains/gh-test4.json") 
-
+	
 	loadAllVMfiles()
-
+	
 	fmt.Println("\n\n\n\n LOADED CONFIG\n\n\n\n")
 	dumpConfig()
 
@@ -90,8 +132,8 @@ func (c *Conf)getVMbyDomain(argDomain *string)(v *VM, err error){
 	return nil,errors.New("conf VM not found")}
 
 
-func (c *Conf)getNodebyNodename(argNodename *string)(v *Node, err error){
+func (c *Conf)getNodebyHostname(argHostname *string)(v *Node, err error){
 	for _,t:= range c.Nodes{
-		if t.Nodename == *argNodename{
+		if t.Hostname == *argHostname{
 			return &t, nil}}
 	return nil,errors.New("conf Node not found")}
--- main.go
+++ main.go
@@ -1,16 +1,42 @@
+/*
+ *  lvcl is a simple program clustering libvirt servers
+ *  Copyright (C) 2020 Adam Prycki (email: adam.prycki@gmail.com)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package main
-
-//import "os"
-
 
 func main(){
 	bar()
 	confLoad()
+
+	brainIN:=make(chan message,10)
+	brainOUT:=make(chan message,10)
+	loggerIN:=make(chan message,10)
+	loggerOUT:=make(chan message,10)
+	
+	e := Exchange{
+		myHostname: config.MyHostname,
+		nodeList: &config.Nodes,
+		brainIN: brainIN,
+		brainOUT: brainOUT,
+		loggerIN: loggerIN,
+		loggerOUT: loggerOUT,}
+	e.initConnections()
+
 	/*
-	name,err:=os.Hostname()
-	if(err != nil){
-		panic(err)
-		}
 	*/
 	
 	//dumpConfig()
